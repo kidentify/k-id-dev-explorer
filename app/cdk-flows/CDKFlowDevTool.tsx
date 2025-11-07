67c6f3b40985a283a35ee202213e59fbcb5d88df
Travis Brown <travis@k-id.com>
2025-11-06 19:18:40 -0500

    fix: rename CDK Explorer to k-ID Dev Explorer

--- app/cdk-flows/CDKFlowDevTool.tsx
+++ app/cdk-flows/CDKFlowDevTool.tsx
@@ -439,7 +439,7 @@ export default function CDKFlowDevTool({ onIframeUrlUpdate, apiKeyStatus, onAddE
                 Loading...
               </>
             ) : (
-              'Embed CDK Flow'
+              'Embed widget'
             )}
           </button>
         </form>
